0626d3e19585cf1cff9d8b4957ced43d7532cb04
James Shawn Carnley <shawn@floatingboy.org>
2022-10-05 21:07:27 -0400

    code cleanup

--- pedalShield_uno_2_0.ino
+++ pedalShield_uno_2_0.ino
@@ -17,13 +17,9 @@
 // defining harware resources.
 int LED = 13;
 int FOOTSWITCH = 12;
-int SIG_A = 0;      // pin A output
-int SIG_B = 0;      // pin B output
-int lastSIG_A = 0;  // last state of SIG_A
-int lastSIG_B = 0;  // last state of SIG_B
-int Pin_BTN = 3;    // GPIO #3-Push button on encoder
-int Pin_A = 4;      // GPIO #4-Pin_A on encoder (Output A)
-int Pin_B = 2;      // GPIO #5-Pin_B on encoder (Output B)
+int Pin_BTN = 3;  // GPIO #3-Push button on encoder
+int Pin_A = 4;    // GPIO #4-Pin_A on encoder (Output A)
+int Pin_B = 2;    // GPIO #5-Pin_B on encoder (Output B)
 
 BfButton btn(BfButton::STANDALONE_DIGITAL, Pin_BTN, true, LOW);
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);  // I2C / TWI
@@ -34,17 +30,22 @@ U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);  // I2C / TWI
 #define PWM_QTY 2        // 2 PWMs in parallel
 
 // other variables
+int SIG_A = 0;                                                                 // pin A output
+int SIG_B = 0;                                                                 // pin B output
+int lastSIG_A = 0;                                                             // last state of SIG_A
+int lastSIG_B = 0;                                                             // last state of SIG_B
 int bank = 1;                                                                  // Initial effect bank
 int bank_max = 3;                                                              // Max number of effect banks
 String bank_names[] = { "C L E A N", "O C T A V E R", "E M P T Y  B A N K" };  // Effect Names
 int pulseCount = 0;                                                            // Rotary pulses
 int read_counter = 0;                                                          // Interrupt read counter
 int ocr_counter = 0;                                                           // Output compare register counter
-int input, vol_variable = 512;                                                 // Signal from instrument
+int input;                                                                     // Signal from instrument
+int vol_variable = 512;                                                        // Volume
 int dist_variable = 250;                                                       // Effect ammount?
 String normalized_output;                                                      // Map output display to a 0-100 scale
 byte ADC_low, ADC_high;                                                        // Analogue to Digital Converter low and high bytes
-FastMap mapper;
+FastMap mapper;                                                                // map() is slow af
 
 //Button press hanlding function
 void pressHandler(BfButton* btn, BfButton::press_pattern_t pattern) {
@@ -116,39 +117,44 @@ void setup() {
 
 void loop() {
   // Turn on the LED if the effect is ON.
-  if (digitalRead(FOOTSWITCH)) {
+  if (digitalRead(FOOTSWITCH)) {  // STOMP
+
     digitalWrite(LED, HIGH);
+
     // Read Rotary Encoder Push Button
     btn.read();
+
+    // Check the rotary encoder for values
     SIG_A = digitalRead(Pin_A);  //read state of A
     SIG_B = digitalRead(Pin_B);  //read state of B
+
+
+    u8g.firstPage();
+    do {
+      draw(bank_names[bank - 1].c_str(), 0, 8);
+      switch (bank) {
+        case (1):
+          mapper.init(0, 1024, long(0), long(100));
+          draw("VOLUME:", 0, 30);
+          normalized_output = String(mapper.map(float(vol_variable)));
+          break;
+
+        case (2):
+          mapper.init(0, 500, long(0), long(100));
+          draw("EFFECT:", 0, 30);
+          normalized_output = String(mapper.map(float(dist_variable)));
+          break;
+
+        default:
+          normalized_output = "";
+      }
+
+      draw(normalized_output.c_str(), 80, 30);
+    } while (u8g.nextPage());
+    // nothing else here, all happens in the TIMER1_CAPT_vect interruption.
   } else {
     digitalWrite(LED, LOW);
   }
-
-  u8g.firstPage();
-  do {
-    draw(bank_names[bank - 1].c_str(), 0, 8);
-    switch (bank) {
-      case (1):
-        mapper.init(0, 1024, long(0), long(100));
-        draw("VOLUME:", 0, 30);
-        normalized_output = String(mapper.map(float(vol_variable)));
-        break;
-
-      case (2):
-        mapper.init(0, 500, long(0), long(100));
-        draw("EFFECT:", 0, 30);
-        normalized_output = String(mapper.map(float(dist_variable)));
-        break;
-
-      default:
-        normalized_output = "";
-    }
-
-    draw(normalized_output.c_str(), 80, 30);
-  } while (u8g.nextPage());
-  // nothing else here, all happens in the TIMER1_CAPT_vect interruption.
 }
 
 void draw(char* str, int posx, int posy) {
@@ -156,79 +162,86 @@ void draw(char* str, int posx, int posy) {
 }
 
 ISR(TIMER1_CAPT_vect) {
-  read_counter++;
-  if (read_counter == 100) {  // do work every 100 reads.
-    read_counter = 0;
-    // Check the rotary encoder for values
-    btn.read();
-    SIG_A = digitalRead(Pin_A);  //read state of A
-    SIG_B = digitalRead(Pin_B);  //read state of B
+  if (digitalRead(FOOTSWITCH)) {  // STOMP
+    read_counter++;
+    if (read_counter == 100) {  // do work every 100 reads.
+      read_counter = 0;
+      // Read Rotary Encoder Push Button
+      btn.read();
 
-    /****************************************
-    * Effect Banks
-    *****************************************/
-    switch (bank) {
-      /* C L E A N */
-      case (1):
-        // get ADC data
-        ADC_low = ADCL;  // you need to fetch the low byte first
-        ADC_high = ADCH;
-        //construct the input sumple summing the ADC low and high byte.
-        input = ((ADC_high << 8) | ADC_low) + 0x8000;  // make a signed 16b value
+      // Check the rotary encoder for values
+      SIG_A = digitalRead(Pin_A);  //read state of A
+      SIG_B = digitalRead(Pin_B);  //read state of B
 
-        if (!(SIG_B == SIG_A) && (lastSIG_B != SIG_B)) {
-          pulseCount--;
-          if (vol_variable > 0) vol_variable--;  //anti-clockwise rotation
-          lastSIG_B = SIG_B;
-        } else if (!(SIG_B != SIG_A) && (lastSIG_B == SIG_B)) {
-          pulseCount++;                             //clockwise rotation
-          if (vol_variable < 1024) vol_variable++;  //clockwise rotation
-          lastSIG_B = SIG_B > 0 ? 0 : 1;            //save last state of B
-        }
-
-        //the amplitude of the signal is modified following the vol_variable using the Arduino map fucntion
-        input = map(input, 0, 1024, 0, vol_variable);
-
-        //write the PWM signal
-        OCR1AL = ((input + 0x8000) >> 8);  // convert to unsigned, send out high byte
-        OCR1BL = input;                    // send out low byte
-        break;
-
-      /* O C T A V E R */
-      case (2):
-        if (!(SIG_B == SIG_A) && (lastSIG_B != SIG_B)) {
-          pulseCount--;
-          if (dist_variable > 0) dist_variable--;  //anti-clockwise rotation
-          lastSIG_B = SIG_B;
-        } else if (!(SIG_B != SIG_A) && (lastSIG_B == SIG_B)) {
-          pulseCount++;                              //clockwise rotation
-          if (dist_variable < 500) dist_variable++;  //clockwise rotation
-          lastSIG_B = SIG_B > 0 ? 0 : 1;             //save last state of B
-        }
-        ocr_counter++;
-        if (ocr_counter >= dist_variable) {
-          ocr_counter = 0;
-
+      /****************************************
+      * Effect Banks
+      *****************************************/
+      switch (bank) {
+        /* C L E A N */
+        case (1):
           // get ADC data
-          ADC_low = ADCL;  // you need to fetch the low byte first
+          ADC_low = ADCL;  // low byte first
           ADC_high = ADCH;
-
-          //construct the input sumple summing the ADC low and high byte.
+          
+          //construct the input summing the ADC low and high byte.
           input = ((ADC_high << 8) | ADC_low) + 0x8000;  // make a signed 16b value
 
+          if (!(SIG_B == SIG_A) && (lastSIG_B != SIG_B)) {
+            pulseCount--;
+            if (vol_variable > 0) vol_variable--;  //anti-clockwise rotation
+            lastSIG_B = SIG_B;
+          } else if (!(SIG_B != SIG_A) && (lastSIG_B == SIG_B)) {
+            pulseCount++;                             //clockwise rotation
+            if (vol_variable < 1024) vol_variable++;  
+            lastSIG_B = SIG_B > 0 ? 0 : 1;            //save last state of B
+          }
+
+          //the amplitude of the signal is modified following the vol_variable using the Arduino map fucntion
+          //UNTESTED! LOOOK AT ME! DON'T FORGET RE-VISIT THIS! GAY PR0N!
+          input = mapper.map(float(vol_variable));
+          //input = map(input, 0, 1024, 0, vol_variable);
+
+          //write the PWM signal
           OCR1AL = ((input + 0x8000) >> 8);  // convert to unsigned, send out high byte
           OCR1BL = input;                    // send out low byte
-        }
-        break;
+          break;
 
-      /* E M P T Y */
-      case (3):
-        break;
+        /* O C T A V E R */
+        case (2):
+          if (!(SIG_B == SIG_A) && (lastSIG_B != SIG_B)) {
+            pulseCount--;
+            if (dist_variable > 0) dist_variable--;  //anti-clockwise rotation
+            lastSIG_B = SIG_B;
+          } else if (!(SIG_B != SIG_A) && (lastSIG_B == SIG_B)) {
+            pulseCount++;                              //clockwise rotation
+            if (dist_variable < 500) dist_variable++;  //clockwise rotation
+            lastSIG_B = SIG_B > 0 ? 0 : 1;             //save last state of B
+          }
+          ocr_counter++;
+          if (ocr_counter >= dist_variable) {
+            ocr_counter = 0;
 
-      /***********/
-      default:  // whoops too far!
-        bank = 1;
-        break;
+            // get ADC data
+            ADC_low = ADCL;  // you need to fetch the low byte first
+            ADC_high = ADCH;
+
+            //construct the input sumple summing the ADC low and high byte.
+            input = ((ADC_high << 8) | ADC_low) + 0x8000;  // make a signed 16b value
+
+            OCR1AL = ((input + 0x8000) >> 8);  // convert to unsigned, send out high byte
+            OCR1BL = input;                    // send out low byte
+          }
+          break;
+
+        /* E M P T Y */
+        case (3):
+          break;
+
+        /***********/
+        default:  // whoops too far!
+          bank = 1;
+          break;
+      }
     }
   }
 }
